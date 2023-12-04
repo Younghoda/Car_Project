d517829360b53ef6a5fe02c6ed047007c7605804
Younghoda <dudgh597@naver.com>
2023-12-04 18:52:35 +0900

    test

---
+++ Car_Project.py
@@ -0,0 +1,281 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+# import pytesseract
+import PIL
+import pytesseract
+
+plt.style.use('dark_background')
+
+# 1. 이미지 읽고
+img = cv2.imread('car3.jpg')
+img = cv2.resize(img, (1080, 600))
+height, width, channel = img.shape
+
+# 2. 이미지 to gray
+# 이 코드는 OpenCV에서 제공하는 cv2.cvtColor() 함수를 사용하여 컬러 이미지를 흑백 또는 그레이스케일 이미지로 변환합니다.
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 가우시안블러 -> thresholding
+# cv2.GaussianBlur 함수는 이미지에 가우시안 필터를 적용하여 이미지를 흐리게 만듭니다.
+# 이 함수의 첫 번째 인자 img는 입력 이미지를 나타내며, 두 번째 인자는 필터의 크기를 결정하는 매개변수입니다.
+# (val, val) 형태로 전달되는데, 이때 val은 필터의 크기를 결정하는 값입니다. 이 값이 클수록 블러 효과가 강해지며, 작을수록 세밀한 부분이 유지됩니다.
+# 마지막 매개변수인 0은 가우시안 필터의 X축과 Y축의 표준편차를 의미하는데, 0으로 설정하면 자동으로 계산됩니다.
+# 결과적으로, 이 코드는 img라는 이미지에 val 크기의 가우시안 필터를 적용하여 이미지를 흐리게 만들어 blur 변수에 저장합니다.
+blurred = cv2.GaussianBlur(imgray, (5, 5), 0)
+# cv2.threshold() 함수는 이미지를 이진화하는 데 사용됩니다.
+# 이미지 이진화는 픽셀 값을 임계값을 기준으로 두 부류(예를 들어, 검은색과 흰색)로 나누는 과정을 말합니다.
+# 이 함수는 주어진 이미지를 특정 임계값보다 큰 값과 작은 값으로 나누어줍니다.
+# 픽셀값이 127보다 크면 255, 작으면 0
+# 픽셀값이 threshold_value보다 크면 value, 작으면 0(위의 주석과 똑같은 말임)
+thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                           cv2.THRESH_BINARY_INV, 19, 9)
+
+# 4. contours
+contours, _ = cv2.findContours(thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+
+# cv2.imshow('1', thresh)
+# cv2.imshow('2', img)
+
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
+
+# 컨투어의 사각형 범위 찾기
+
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+contours_dict = []
+
+for contour in contours:
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(temp_result, pt1=(x, y), pt2=(x + w, y + h), color=(255, 255, 255), thickness=2)
+
+    # insert to dict
+    contours_dict.append({
+        'contour': contour,
+        'x': x,
+        'y': y,
+        'w': w,
+        'h': h,
+        'cx': x + (w / 2),
+        'cy': y + (h / 2)
+    })
+
+# 어떤게 번호판처럼 생겼는지?
+
+MIN_AREA = 80
+MIN_WIDTH, MIN_HEIGHT = 2, 8
+MIN_RATIO, MAX_RATIO = 0.25, 1.0
+
+possible_contours = []
+
+cnt = 0
+for d in contours_dict:
+    area = d['w'] * d['h']
+    ratio = d['w'] / d['h']
+
+    if area > MIN_AREA \
+            and d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT \
+            and MIN_RATIO < ratio < MAX_RATIO:
+        d['idx'] = cnt
+        cnt += 1
+        possible_contours.append(d)
+
+# visualize possible contours
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+for d in possible_contours:
+    #     cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
+    cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x'] + d['w'], d['y'] + d['h']), color=(255, 255, 255),
+                  thickness=2)
+
+# 리얼 번호판 추려내기
+MAX_DIAG_MULTIPLYER = 5  # 5
+MAX_ANGLE_DIFF = 12.0  # 12.0
+MAX_AREA_DIFF = 0.5  # 0.5
+MAX_WIDTH_DIFF = 0.8
+MAX_HEIGHT_DIFF = 0.2
+MIN_N_MATCHED = 3  # 3
+
+
+def find_chars(contour_list):
+    matched_result_idx = []
+
+    for d1 in contour_list:
+        matched_contours_idx = []
+        for d2 in contour_list:
+            if d1['idx'] == d2['idx']:
+                continue
+
+            dx = abs(d1['cx'] - d2['cx'])
+            dy = abs(d1['cy'] - d2['cy'])
+
+            diagonal_length1 = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
+
+            distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
+            if dx == 0:
+                angle_diff = 90
+            else:
+                angle_diff = np.degrees(np.arctan(dy / dx))
+            area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
+            width_diff = abs(d1['w'] - d2['w']) / d1['w']
+            height_diff = abs(d1['h'] - d2['h']) / d1['h']
+
+            if distance < diagonal_length1 * MAX_DIAG_MULTIPLYER \
+                    and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
+                    and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
+                matched_contours_idx.append(d2['idx'])
+
+        # append this contour
+        matched_contours_idx.append(d1['idx'])
+
+        if len(matched_contours_idx) < MIN_N_MATCHED:
+            continue
+
+        matched_result_idx.append(matched_contours_idx)
+
+        unmatched_contour_idx = []
+        for d4 in contour_list:
+            if d4['idx'] not in matched_contours_idx:
+                unmatched_contour_idx.append(d4['idx'])
+
+        unmatched_contour = np.take(possible_contours, unmatched_contour_idx)
+
+        # recursive
+        recursive_contour_list = find_chars(unmatched_contour)
+
+        for idx in recursive_contour_list:
+            matched_result_idx.append(idx)
+
+        break
+
+    return matched_result_idx
+
+
+result_idx = find_chars(possible_contours)
+
+matched_result = []
+for idx_list in result_idx:
+    matched_result.append(np.take(possible_contours, idx_list))
+
+# visualize possible contours
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+for r in matched_result:
+    for d in r:
+        #         cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
+        cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x'] + d['w'], d['y'] + d['h']), color=(255, 255, 255),
+                      thickness=2)
+
+# 똑바로 돌리기
+PLATE_WIDTH_PADDING = 1.3  # 1.3
+PLATE_HEIGHT_PADDING = 1.5  # 1.5
+MIN_PLATE_RATIO = 3
+MAX_PLATE_RATIO = 10
+
+plate_imgs = []
+plate_infos = []
+
+for i, matched_chars in enumerate(matched_result):
+    sorted_chars = sorted(matched_chars, key=lambda x: x['cx'])
+
+    plate_cx = (sorted_chars[0]['cx'] + sorted_chars[-1]['cx']) / 2
+    plate_cy = (sorted_chars[0]['cy'] + sorted_chars[-1]['cy']) / 2
+
+    plate_width = (sorted_chars[-1]['x'] + sorted_chars[-1]['w'] - sorted_chars[0]['x']) * PLATE_WIDTH_PADDING
+
+    sum_height = 0
+    for d in sorted_chars:
+        sum_height += d['h']
+
+    plate_height = int(sum_height / len(sorted_chars) * PLATE_HEIGHT_PADDING)
+
+    triangle_height = sorted_chars[-1]['cy'] - sorted_chars[0]['cy']
+    triangle_hypotenus = np.linalg.norm(
+        np.array([sorted_chars[0]['cx'], sorted_chars[0]['cy']]) -
+        np.array([sorted_chars[-1]['cx'], sorted_chars[-1]['cy']])
+    )
+
+    angle = np.degrees(np.arcsin(triangle_height / triangle_hypotenus))
+
+    rotation_matrix = cv2.getRotationMatrix2D(center=(plate_cx, plate_cy), angle=angle, scale=1.0)
+
+    img_rotated = cv2.warpAffine(thresh, M=rotation_matrix, dsize=(width, height))
+
+    img_cropped = cv2.getRectSubPix(
+        img_rotated,
+        patchSize=(int(plate_width), int(plate_height)),
+        center=(int(plate_cx), int(plate_cy))
+    )
+
+    if img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO or img_cropped.shape[1] / img_cropped.shape[
+        0] < MIN_PLATE_RATIO > MAX_PLATE_RATIO:
+        continue
+
+    plate_imgs.append(img_cropped)
+    plate_infos.append({
+        'x': int(plate_cx - plate_width / 2),
+        'y': int(plate_cy - plate_height / 2),
+        'w': int(plate_width),
+        'h': int(plate_height)
+    })
+
+# 최종확인
+longest_idx, longest_text = -1, 0
+plate_chars = []
+
+for i, plate_img in enumerate(plate_imgs):
+    plate_img = cv2.resize(plate_img, dsize=(0, 0), fx=1.6, fy=1.6)
+    _, plate_img = cv2.threshold(plate_img, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # find contours again (same as above)
+    contours, _ = cv2.findContours(plate_img, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+
+    plate_min_x, plate_min_y = plate_img.shape[1], plate_img.shape[0]
+    plate_max_x, plate_max_y = 0, 0
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+
+        area = w * h
+        ratio = w / h
+
+        if area > MIN_AREA \
+                and w > MIN_WIDTH and h > MIN_HEIGHT \
+                and MIN_RATIO < ratio < MAX_RATIO:
+            if x < plate_min_x:
+                plate_min_x = x
+            if y < plate_min_y:
+                plate_min_y = y
+            if x + w > plate_max_x:
+                plate_max_x = x + w
+            if y + h > plate_max_y:
+                plate_max_y = y + h
+
+    img_result = plate_img[plate_min_y:plate_max_y, plate_min_x:plate_max_x]
+
+    img_result = cv2.GaussianBlur(img_result, ksize=(3, 3), sigmaX=0)
+    _, img_result = cv2.threshold(img_result, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    img_result = cv2.copyMakeBorder(img_result, top=10, bottom=10, left=10, right=10, borderType=cv2.BORDER_CONSTANT,
+                                    value=(0, 0, 0))
+
+    chars = pytesseract.image_to_string(img_result, lang='kor')
+
+    result_chars = ''
+    has_digit = False
+    for c in chars:
+        if ord('가') <= ord(c) <= ord('힣') or c.isdigit():
+            if c.isdigit():
+                has_digit = True
+            result_chars += c
+
+    print(result_chars)
+    plate_chars.append(result_chars)
+
+    if has_digit and len(result_chars) > longest_text:
+        longest_idx = i
+
+    plt.subplot(len(plate_imgs), 1, i + 1)
+    plt.imshow(img_result, cmap='gray')
+    plt.show()
